57428df1fc8a6132d2887fc86cfd4e2ee4f397c3
Funmi Adewodu <funmiadewodu@yahoo.com>
2017-06-17 12:04:48 +0000

    Added solution to challenge 2:8

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -5,3 +5,14 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+
+def got_three?(elements)
+  elements.each_cons(3) do |a, b, c|
+    return true if a == b && b == c
+  end
+  false
+end
+
+
+#note: each_cons() provides a block with consective numbers of an enumerator 
