b3380507ebb86311496be3bf506a18fc80c62cd1
Funmi Adewodu <funmiadewodu@yahoo.com>
2017-06-18 19:33:45 +0000

    Added solution to challenge 2:11

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -18,3 +18,46 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+
+class BeerSong 
+  attr_accessor "beers"
+  
+  def initialize(beers)
+    beers = 0 if beers < 0
+    beers = 99 if beers > 99 
+    self.beers = beers 
+  end 
+  
+  def print_song
+    while beers > 0 && beers != 0
+      puts	"#{translate(beers)} #{bottle(beers)} of beer on the wall,"
+      puts	"#{translate(beers)} #{bottle(beers)} of beer,"
+      puts "Take one down, pass it around,"
+      puts "#{translate(beers - 1)} #{bottle(beers - 1)} of beer on the wall."
+    
+    self.beers -= 1
+    end 
+  end 
+  
+  # returns "bottle" or "bottles"
+  def bottle(beers)
+    if beers == 1 
+      "bottle"
+    else 
+      "bottles"
+    end 
+  end 
+  
+  # translates numbers to english
+  def translate(beers)
+    if 0 <= beers && beers <= 19
+      %w(zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)[beers]
+    elsif beers % 10 == 0
+      %w(zero ten twenty thirty forty fifty sixty seventy eighty ninety)[beers/10]
+    else
+      "#{translate beers/10*10}-#{translate beers%10}".downcase
+    end.capitalize
+  end 
+  
+end 
